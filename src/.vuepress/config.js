6ea2a1631c6a04597e5468f87bf3b43269dc2846
c3po <iotac3po@gmail.com>
2021-12-17 16:29:57 +0100

    clean

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -113,5 +113,9 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    '@vuepress/plugin-google-analytics',
+     {
+       id: 'G-01CJP2RX6Q',
+     },
   ]
 }
